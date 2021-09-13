4bcb11952740d8fa3af28b22aed5884b17d3c6f5
plusmouse <plusmouse@protonmail.com>
2021-09-13 14:55:34 +0100

    Add export CSV dialog to buy screens

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -16,6 +16,7 @@ AtrL["SCAN_EXPLANATION"] = "Во время сканирование аукци�
 
 -- New Vars
 --- Globals
+AUCTIONATOR_L_COPY_TEXT = "Copy Text"
 AUCTIONATOR_SELL_TAB_TEXT = "Sell"
 AUCTIONATOR_AUCTION_DURATION = "Default Auction Duration"
 AUCTIONATOR_DEFAULT_AUCTIONATOR_TAB = "Default Auctionator tab"
